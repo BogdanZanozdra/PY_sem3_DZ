e45a787670d418e67a6e4ea5bd11f694d4af32eb
Bogdan Zanozdra <bz300788@gmail.com>
2022-09-15 18:13:05 +0300

    sem 3

---
+++ task_2.py
@@ -0,0 +1,23 @@
+# 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент,
+# второй и предпоследний и т.д.
+# Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+my_list = [2, 4, 8, 5, 3, 1, 8]
+
+def mult_pairs(listt):
+    res = []
+    last_index = len(listt) - 1
+    if len(listt) % 2 != 0:
+        for i in range(len(listt) // 2 + 1):
+            res.append(listt[i] * listt[last_index])
+            last_index -= 1
+        return res
+    else:
+        for i in range(len(listt) // 2):
+            res.append(listt[i] * listt[last_index])
+            last_index -= 1
+        return res
+
+print(f'Произведения пар чисел списка: {mult_pairs(my_list)}')
---
+++ task_5.py
@@ -0,0 +1,28 @@
+# 5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+# Пример:
+# для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+from typing import List
+
+n = int(input('Задайте число ряда Фибоначчи: \n'))
+
+def fibonachi(n): 
+  fib1 = fib2 = 1
+  fib_list = [1, 1]
+  for i in range(2, n):
+      fib1, fib2 = fib2, fib1 + fib2
+      fib_list.append(fib2)
+  return fib_list
+
+def negative_fib(neg_list):
+  temp = neg_list.copy()
+  neg_list.reverse()
+  neg_list.append(0)
+  neg_list = neg_list + temp
+  i = 0
+  while i  <= n:
+    neg_list[i] *= -1
+    i += 2
+  return neg_list
+
+print(fibonachi(n))
+print(negative_fib(fibonachi(n)))
